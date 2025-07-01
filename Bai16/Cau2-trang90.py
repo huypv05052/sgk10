2d6bc3936e3774b7e66ce065944a7e4b22b9447e
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-01 18:27:25 +0700

    Update Cau2-trang90.py

--- Bai16/Cau2-trang90.py
+++ Bai16/Cau2-trang90.py
@@ -1,1 +1,19 @@
+#Viết chương trình Python in ra màn hình bảng nhân trong phạm vi 10
+# Method 1
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(str(j)+'x'+str(i)+'='+str(j*i), end='\t')
+    print()
 
+
+# Method 2: use f-string
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f'{j}x{i:1}={j*i:2}', end='\t')
+    print()
+
+
+#Bonus: f-string:
+name = 'Huy'
+age = 29
+print(f'Hello my name is {name}, I am {age}')
