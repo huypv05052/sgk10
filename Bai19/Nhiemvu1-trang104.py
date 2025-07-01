e57b96035f833da22cd5bebe600d65de47a6aa3e
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-01 23:34:34 +0700

    Update Nhiemvu1-trang104.py

--- Bai19/Nhiemvu1-trang104.py
+++ Bai19/Nhiemvu1-trang104.py
@@ -19,3 +19,22 @@ print(f'Số tiền phải trả là: {t:.2f} nghìn đồng')
 
 
 # Method 2
+def tienDien():
+    while True:
+        try: 
+            k = int(input('Nhập số kWh điện tiêu thụ: '))
+            if k < 0: 
+                print('Số điện tiêu thụ không được âm. Vui lòng nhập lại!')
+                continue
+            break           
+        except ValueError:
+            print('Lỗi! Dữ liệu nhập vào không đúng!')
+    if k < 51: 
+        tien = k*1.678
+    elif (k>=51 and k < 101): 
+        tien = k*1.734
+    else: 
+        tien = k*2.014
+    return f'Số tiền phải trả là {tien:.2f} nghìn đồng'
+
+print(tienDien())
