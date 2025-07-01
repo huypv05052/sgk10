70cbb5ec6f0d5644c36bbfcb645145c765d8e35c
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-01 16:53:28 +0700

    Update Bai2-trang90.py

--- Bai2-trang90.py
+++ Bai2-trang90.py
@@ -1,1 +1,5 @@
-
+#Viết chương trình Python in ra màn hình bảng nhân trong phạm vi 10
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(str(j)+'x'+str(i)+'='+str(j*i), end='\t')
+    print()
