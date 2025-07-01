0ef0245c316c475827c56d61724c06fbce77108c
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-01 22:46:57 +0700

    Update Cau1-trang100.py

--- Bai18/Cau1-trang100.py
+++ Bai18/Cau1-trang100.py
@@ -10,3 +10,34 @@ p = (a + b + c)/2
 s = math.sqrt(p*(p-a)*(p-b)*(p-c))
 print(f'Chu vi tam giác bằng: {a+b+c}')
 print(f'Diện tích tam giác bằng: {s}')
+
+
+# Method 2
+import math
+def tamGiac():
+    while True:
+        try:
+            a = float(input('a = '))
+            b = float(input('b = '))
+            c = float(input('c = '))
+
+            #check a, b, c is ok:
+            if (a <= 0) or (b <= 0) or (c <= 0):
+                print('Vui lòng nhập số thực dương!')
+                continue
+            if (a+b <= c) or (a+c <= b) or (b+c <= a):
+                print('Ba cạnh không tạo thành tam giác hợp lệ! Vui lòng nhập lại!')
+                continue
+            break   #Dữ liệu hợp lệ! Thoát vòng lặp 
+        except ValueError:
+            print('Lỗi: Vui lòng nhập số thực!')
+
+    #Tính chu vi, diện diện tích
+    chu_vi = a+b+c
+    p = chu_vi/2
+    dien_tich = math.sqrt(p*(p-a)*(p-b)*(p-c))
+
+    print(f'Chu vi tam giác bằng: {chu_vi}')
+    print(f'Diện tích tam giác bằng: {dien_tich:.2f}')
+
+tamGiac()
