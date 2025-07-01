96de290b6df5e291c99550c36ff4f5e1c61cc59a
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-01 18:40:58 +0700

    Update Cau1-trang100.py

--- Bai18/Cau1-trang100.py
+++ Bai18/Cau1-trang100.py
@@ -1,1 +1,12 @@
+#Viết chương trình nhập 3 số thực dương a, b, c và tính chu vi, diện tích tam giác có độ dài ba cạnh là a, b, c
+# suggest: S = sqrt(p(p-a)(p-b)(p-c)) , p là nửa chu vi
+# Method 1
+import math
+a = int(input('a='))
+b = int(input('b='))
+c = int(input('c='))
 
+p = (a + b + c)/2
+s = math.sqrt(p*(p-a)*(p-b)*(p-c))
+print(f'Chu vi tam giác bằng: {a+b+c}')
+print(f'Diện tích tam giác bằng: {s}')
