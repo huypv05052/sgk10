bb5b62b8be4e9882085aa931b7b3118f98652032
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-02 23:41:01 +0700

    Update Cau1-trang107.py

--- Bai20/Cau1-trang107.py
+++ Bai20/Cau1-trang107.py
@@ -1,1 +1,18 @@
+#Viết chương trình tính tổng: S = 1 + 1/2 + 1/3 + ... + 1/n
+def tinhTong():
+    while True:
+        try:
+            n = int(input('n = '))
+            if n <= 0:
+                print('Nhập số nguyên dương.')
+                continue
+            break
+        except ValueError:
+            print('Nhập số nguyên dương!')
+    
+    S = 0
+    for i in range(1, n+1):
+        S += 1/i
+    return f'Tổng S = {S:.2f}'
 
+print(tinhTong())
