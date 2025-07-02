329671bf8b96f99bbc87d699b4b750517f920b34
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-02 23:34:18 +0700

    Update Nhiemvu1+2-trang107.py

--- Bai20/Nhiemvu1+2-trang107.py
+++ Bai20/Nhiemvu1+2-trang107.py
@@ -1,1 +1,22 @@
+#Nhiệm vụ 1 và 2: Nhập số tự nhiên n từ bàn phím và in ra và đếm dãy các ước của n theo chiều ngang màn hình
+def demUoc():
+    while True:
+        try:
+            n = int(input('n = '))
+            if n <= 0:
+                print('Vui lòng nhập số nguyên dương!')
+                continue
+            break
+        except ValueError:
+            print('Vui lòng nhập số nguyên dương!')
+    
+    result = f'Các ước số của {n} là: '
+    count = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            result += f'{i} '
+            count += 1
+    result += f'\nTổng cộng có {count} ước của {n}'
+    return result
 
+print(demUoc())
