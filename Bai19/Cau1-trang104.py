12e5e0b796d324c5f221243e7491f57ad6e2c9cf
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-02 19:04:26 +0700

    Update Cau1-trang104.py

--- Bai19/Cau1-trang104.py
+++ Bai19/Cau1-trang104.py
@@ -1,1 +1,22 @@
+#Giá bán cam tại siêu thị như sau
+#Nếu mua dưới 5kg: 12k/kg
+#nếu mua lớn hơn 5kg: 10k/kg
+#Viết chương trình nhập số lượng mua, sau đó tính số tiền phải trả
 
+
+def tinhTien():
+    while True:
+        try:
+            k = float(input('Nhập số kg đã mua: '))
+            if k <= 0:
+                print('Số kg không hợp lệ! Vui lòng nhập lại!')
+                continue
+            break
+        except ValueError:
+            print('Lỗi! Số kg nhập vào không hợp lệ!')
+    if k <= 5:
+        tien = k*12
+    else:
+        tien = k*10
+    return f'Số tiền cần trả là: {tien} nghìn đồng'
+print(tinhTien())
