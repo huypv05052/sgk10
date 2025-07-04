b03c42834da40a1e6bc2f3e6c60cbbcffed87a77
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-04 21:28:47 +0700

    Update Cau1+2-trang114.py

--- Bai22/Cau1+2-trang114.py
+++ Bai22/Cau1+2-trang114.py
@@ -1,1 +1,17 @@
-
+#Viết lệnh xóa phần tử cuối của danh sách, thêm phần tử vào đầu danh sách
+while True:
+    try:
+        n=int(input('n='))
+        if n>0:
+            break
+        else:
+            print('Nhập số nguyên dương.')
+    except ValueError:
+        print('Nhập số nguyên dương.')
+A=[]
+for i in range(n):
+    element=input(f'Nhập phần tử A[{i+1}]: ')
+    A.append(element)
+A.pop()
+A.insert(0, input('Nhập giá trị A[0]: '))
+print(*A)
