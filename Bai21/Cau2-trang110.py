b4ce73f59d589e3f3cad4890f7d4c278fb51ee75
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-04 17:21:41 +0700

    Update Cau2-trang110.py

--- Bai21/Cau2-trang110.py
+++ Bai21/Cau2-trang110.py
@@ -1,1 +1,9 @@
+#Viết chương trình đếm trong 100 số tự nhiên đầu tiên có bao nhiêu số thỏa điều kiện: chia hết 5 hoặc chia cho 3 dư 1
 
+count = 0
+i = 0
+while i <= 100:
+    if (i % 5 == 0) or (i%3==1):
+        count+=1
+    i += 1
+print(count)
