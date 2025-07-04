97007dd5237faff83eb816c391467e752d0c0494
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-04 21:42:58 +0700

    Update Vandung-trang114.py

--- Bai22/Vandung-trang114.py
+++ Bai22/Vandung-trang114.py
@@ -1,1 +1,28 @@
-
+#Nhập dãy số A. viết chương trình tìm giá trị và chỉ số của phần tử lớn nhất và nhỏ nhất của A.
+def maxMinA():
+    while True:
+        try:
+            n=int(input('n='))
+            if n>0:
+                break
+            else:
+                print('Nhập số nguyên dương.')
+        except ValueError:
+            print('Nhập số nguyên dương.')
+    A=[]
+    for i in range(n):
+        while True:
+            try:
+                number=int(input(f'Nhập giá trị phần tử A[{i+1}]: '))
+                A.append(number)
+                break
+            except ValueError:
+                print('Nhập giá trị là một số nguyên.')
+    print(*A)
+    maxValue=max(A)
+    maxIndex=A.index(maxValue)
+    minValue=min(A)
+    minIndex=A.index(minValue)
+    print(f'Phần tử lớn nhất là A[{maxIndex}]: {maxValue}')
+    print(f'Phần tử nhỏ nhất là A[{minIndex}]: {minValue}')
+maxMinA()
