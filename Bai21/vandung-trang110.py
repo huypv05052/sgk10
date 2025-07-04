050b2b68a9e4f87dced22ca0183154f017fc2c49
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-04 17:35:43 +0700

    Update vandung-trang110.py

--- Bai21/vandung-trang110.py
+++ Bai21/vandung-trang110.py
@@ -1,1 +1,16 @@
+#Viết chương trình in các số tự nhiên 1 tới 100, in thành 10 hàng, mỗi hàng 10 số
+#while loop:
+i = 1
+while i <= 100:
+    if i%10==0:
+        print(i)
+    else:
+        print(i, end='\t')
+    i+=1
 
+
+#for loop:
+for i in range(1, 101):
+    print(i, end=' ')
+    if i%10==0:
+        print()
