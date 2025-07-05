633cc6da0c4627346409a2f2343db1073439b352
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-05 17:20:23 +0700

    Update Nhiemvu2-trang117.py

--- Bai23/Nhiemvu2-trang117.py
+++ Bai23/Nhiemvu2-trang117.py
@@ -1,1 +1,31 @@
+#Cho list A chứa số nguyên. Viết chương trình xóa đi các phần tử có giá trị âm trong A
+def positiveA():
+    while True:
+        try:
+            n=int(input('n='))
+            if n>0:
+                break
+            else:
+                print('Enter positive number.')
+        except ValueError:
+            print('Enter positive number.')
 
+    A=[]
+    for i in range(n):
+        while True:
+            try:
+                num=int(input(f'Enter the {i+1}-th number: '))
+                A.append(num)
+                break
+            except ValueError:
+                print('Enter interact number.')
+    #Delete negative number in A
+    k=0
+    while k<len(A):
+        if A[k]<0:
+            A.remove(A[k])
+        else: 
+            k+=1
+    print(*A)
+
+positiveA()
