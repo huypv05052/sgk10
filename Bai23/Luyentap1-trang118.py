7c23da24f670ac5478256d34cf040410f2911d88
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-06 17:32:03 +0700

    Update Luyentap1-trang118.py

--- Bai23/Luyentap1-trang118.py
+++ Bai23/Luyentap1-trang118.py
@@ -1,12 +1,26 @@
 #Cho dãy [1, 2, 2, 3, 4, 5, 5]. Viết lệnh thưc hiện:
-#a) Chèn số 1 vào ngay sau giá trị 1 của dãy
+#a) Chèn số 2 vào ngay sau các giá trị 1 của dãy
 #b) Chèn số 3 và 4 vào danh sách để dãy số có 3 và 4 liền nhau hai lần
 
-lst=[1, 2, 2, 3, 4, 5, 5, 3, 4]
 #a)
-index_1=lst.index(1)    #Tìm chỉ số của số 1
-lst.insert(index_1+1, 1)        #Sau số 1, chèn số 1
-print(lst)
+# index_1=lst.index(1)    #Tìm chỉ số của số 1
+# lst.insert(index_1+1, 1)        #Sau số 1, chèn số 1
+# print(lst)
+lst=[1, 5, 3, 4, 5, 1, 5, 3, 1, 4]
+vi_tri_chen_2=[]
+i=0
+while i<len(lst)-1:
+    if lst[i]==1:
+        vi_tri_chen_2.append(i+1)
+        i+=2
+    i+=1
+print(f'Vị trí cần chèn 2: {vi_tri_chen_2}')
+for k in reversed(vi_tri_chen_2):
+    lst.insert(k, 2)
+print(f'Danh sách sau khi chèn 2: {lst}')
+
+
+
 
 
 #b) 
@@ -16,18 +30,16 @@ print(lst)
 #         vi_tri_chen = i+2
 #         break
 lst=[1, 2, 2, 3, 4, 5, 5, 3, 4, 1, 3, 4]
-vi_tri_chen=[]
+vi_tri_chen_34=[]
 i=0
 while i<len(lst)-1:
     if lst[i]==3 and lst[i+1]==4:
-        vi_tri_chen.append(i+2)
+        vi_tri_chen_34.append(i+2)
         i+=2
-    else:
-        i+=1
-print(vi_tri_chen)
-
-for vt in reversed(vi_tri_chen):
-    lst.insert(vt, 3)
-    lst.insert(vt+1, 4)
-
-print(lst)
+    i+=1
+print(f'Vị trí cần chèn 3, 4: {vi_tri_chen_34}')
+#chèn 3, 4
+for k in reversed(vi_tri_chen_34):
+    lst.insert(k, 3)
+    lst.insert(k+1, 4)
+print(f'Danh sách sau khi chèn 3, 4: {lst}')
