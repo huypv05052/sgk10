60188ada1bcb13ecaad821e7b6b9830e337d0541
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-31 23:34:23 +0700

    Update Vandung1-trang148.py

--- Bai30/Vandung1-trang148.py
+++ Bai30/Vandung1-trang148.py
@@ -30,7 +30,7 @@ def bubble_sort(A):
     n=len(A)
     for i in range(n-1):
         for j in range(n-i-1):
-            if A[j]>A[j+1]:
+            if A[j]>A[j+1]:            #Giảm thì A[j]<A[j+1]
                 A[j],A[j+1]=A[j+1],A[j]
 
 bubble_sort(A)
