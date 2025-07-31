6c52b5d0eabb7c2a717ffce56865cb572f20d528
huypv05052 <141921167+huypv05052@users.noreply.github.com>
2025-07-31 23:20:28 +0700

    Create Vandung1-trang148.py

---
+++ Bai30/Vandung1-trang148.py
@@ -0,0 +1,37 @@
+#Thuật toán sắp xếp
+#Bubble sort
+
+
+
+while True:
+    try:
+        n=int(input('n='))
+        if n>0:
+            break
+        else:
+            print('Chưa hợp lệ.')
+    except ValueError:
+        print('Chưa hợp lệ.')
+
+A=[]
+for i in range(n):
+    while True:
+        try:
+            number=int(input(f'Nhập giá trị cho phần tử A[{i}]: '))
+            A.append(number)
+            break
+        except ValueError:
+            print('Nhập số nguyên.')
+print(f'Đã nhập danh sách A: {A}')
+
+#sắp xếp A
+
+def bubble_sort(A):
+    n=len(A)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if A[j]>A[j+1]:
+                A[j],A[j+1]=A[j+1],A[j]
+
+bubble_sort(A)
+print(f'Danh sách A sau khi sắp xếp: {A}')
